a5d7583e757449e3ba7e4bd6193b7f63b7aaf5f6
yauhenikharytonchyk <evgeny200224@gmail.com>
2024-01-02 11:52:56 +0100

    First commit

---
+++ src/Seasons_from_month_Switch.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Seasons_from_month_Switch {
+    public static void main(String[] args) {
+        // Задание №1
+        Scanner scan = new Scanner(System.in);
+        String Winter = "Зима";
+        String Spring = "Весна";
+        String Summer = "Лето";
+        String Autumn = "Осень";
+        String other = "Такого месяца не существует";
+        String num = "Введите номер месяца";
+        System.out.print(num);
+
+        int MonthNum = scan.nextInt();
+        switch (MonthNum) {
+            case 12, 1,2 -> System.out.println(Winter);
+            case 3, 4, 5 -> System.out.println(Spring);
+            case 6, 7, 8 -> System.out.println(Summer);
+            case 9, 10,11 -> System.out.println(Autumn);
+            default -> System.out.println(other);
+
+        }
+    }
+}
